c1f05bb5b498907314f0e5a5dfd794a9e89ebd59
Minovel2 <103889388+Minovel2@users.noreply.github.com>
2024-08-04 12:18:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,123 +1,204 @@
 import {world, Player, Entity, system, MolangVariableMap} from "@minecraft/server"
-let over = world.getDimension("overworld");
+const over = world.getDimension("overworld");
 
-//---------------------
-// пример импорта и создания луча
-/*
-import {world, system} from "@minecraft/server"
-let over = world.getDimension("overworld");
-import {Ray} from "./Ray.js"
-
-system.runInterval(() => {
-    for (let p of world.getAllPlayers()) {
-        p.createRay(10, (obj) => {
-        over.runCommandAsync(`particle minecraft:basic_flame_particle ${obj.x} ${obj.y} ${obj.z}`);
-    } );
-    }
-},19)
-*/
-//---------------------
-
-// снизу конструктор лучей и функции для луча, если не знаете, что это такое , то лучше не трогать.
-export class Ray {
-    static raycast = new Set();
-    static getCount() {
-        return Ray.raycast.size;
-    }
-    constructor(maxSteps, x, y, z, tpx, tpy, tpz, func, options = {} ) {
+export class Options {
+    constructor(options) {
+        if (options?.moveInRealTime) {
+            this._moveFunction = _time;
+        } else {
+            this._moveFunction = _tick;
+        }
         
-        this.step = 0;
-        this.maxSteps = maxSteps;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.tpx = tpx * (options?.multiply || 1);
-        this.tpy = tpy * (options?.multiply || 1);
-        this.tpz = tpz * (options?.multiply || 1);
-        this.tpLength = Math.sqrt(tpx**2 + tpy**2 + tpz**2) * (options?.multiply || 1);
-        this.func = func;
+        this.name = options?.name || "";
+        this.moveInRealTime = options?.moveInRealTime || false;
+        this.maxSteps = options?.maxSteps || 10;
+        this.func = options?.func || function() {};
         this.dimension = options?.dimension || over;
         this.source = options?.source;
         this.ignoreBlocks = options?.ignoreBlocks || false;
         this.stepsPerTick = options?.stepsPerTick || 1;
-        this.autoMove = options?.autoMove || true;
+        this.stepsPerSecond = options?.stepsPerSecond || 1;
+        this.canMove = options?.canMove || true;
         this.onDeathFunc = options?.onDeathFunc || function() {};
-        
-        if (options?.l) {
-            this.rotate(options.l);
-        }
+        this.multiply = options?.multiply || 1;
+        this.l = options?.l || 0;
+    }
+    
+    clone() {
+        const c = Object.create(Object.getPrototypeOf(this));
+        for (const key in this) {
+            c[key] = this[key];
+        };
+        return c;
+    }
+}
+
+export class OptimizedRay {
+    static getCount() {
+        return Ray.raycast.size;
+    }
+    
+    constructor(location, velocity, options = new Options()) {
+        this.options = options;
+        this.steps = 0;
+        this._tick = 0;
+        this.isActive = true;
+        this.canMove = options.canMove;
+        this.dimension = options.dimension;
+        this.source = options.source;
+        this.setLocation(location);
+        this.setVelocity(velocity, options.multiply);
         Ray.raycast.add(this);
     }
-  
-  rotate(l) {
-      let rad = l * Math.PI / 180;
-      let x = this.tpx * Math.cos(rad) - this.tpz * Math.sin(rad);
-      let z = this.tpx * Math.sin(rad) + this.tpz * Math.cos(rad);
-      this.tpx = x;
-      this.tpz = z;
+    
+    kill() {
+        this.options.onDeathFunc(this);
+        this.delete();
+    }
+    
+    sleep() {
+        this.isActive = false;
+        Ray.raycast.delete(this);
+    }
+    
+    awake() {
+        this.isActive = true;
+        Ray.raycast.add(this);
+    }
+    
+    setLocation(location = {x: 0, y: 0, z: 0}) {
+        this.location = {
+            x: location.x,
+            y: location.y,
+            z: location.z
+        };
+    }
+    
+    clone() {
+        const c = Object.create(Object.getPrototypeOf(this));
+        for (const key in this) {
+            c[key] = this[key];
+        };
+        c.setLocation(this.location);
+        c.setVelocity(this.velocity);
+        Ray.raycast.add(c);
+        return c;
+    }
+    
+    rotate(l) {
+      const rad = l * Math.PI / 180;
+      const x = this.velocity.x * Math.cos(rad) - this.velocity.z * Math.sin(rad);
+      const z = this.velocity.x * Math.sin(rad) + this.velocity.z * Math.cos(rad);
+      this.velocity.x = x;
+      this.velocity.z = z;
   }
   
   move(useFunc = false) {
     try {
         let block = false;
         
-        if (!this.ignoreBlocks) {
-            block = this.dimension.getBlockFromRay({
-                x: this.x,
-                y: this.y,
-                z: this.z
-            }, {
-                x: this.tpx,
-                y: this.tpy,
-                z: this.tpz
-            }, {
+        if (!this.options.ignoreBlocks) {
+            block = this.dimension.getBlockFromRay(this.location, this.velocity, {
                 maxDistance: this.tpLength * 1.1
             });
         }
         if (!block) {
-        this.x += this.tpx;
-        this.y += this.tpy;
-        this.z += this.tpz;
+        this.location.x += this.velocity.x;
+        this.location.y += this.velocity.y;
+        this.location.z += this.velocity.z;
         
         if (useFunc)
-        this.func(this);
+        this.options.func(this);
         
-        this.step += 1;
+        this.steps += 1;
         
         } else {
-            this.onDeathFunc();
-            Ray.raycast.delete(this);
+            this.kill();
         }
-        if (this.step >= this.maxSteps) {
-        this.onDeathFunc();
-        Ray.raycast.delete(this);
+        if (this.steps >= this.options.maxSteps) {
+        this.kill();
     }
-    } catch {
-        Ray.raycast.delete(this);
+    } catch (e) {
+        console.warn(e);
+        this.delete();
     }
-}
 }
 
-Ray.prototype.delete = function() {
+  setVelocity(velocity = {x: 0, y: 0, z: 0}, multiply = 1) {
+      this.velocity = {
+            x: velocity.x * multiply,
+            y: velocity.y * multiply,
+            z: velocity.z * multiply
+        };
+        
+        this.tpLength = Math.sqrt(this.velocity.x**2 + this.velocity.y**2 + this.velocity.z**2) * multiply;
+  }
+}
+
+export class Ray extends OptimizedRay {
+    static raycast = new Set();
+    static getCount() {
+        return Ray.raycast.size;
+    }
+    constructor(location, velocity, options = new Options()) {
+        super(location, velocity, options);
+        this.options = this.options.clone();
+       /* if (options.l) {
+            this.rotate(options.l);
+        }*/
+    }
+    
+    clone() {
+        const c = super.clone();
+        c.options = this.options?.clone();
+        return c;
+    }
+}
+
+OptimizedRay.prototype.delete = function() {
+    this.isActive = false;
     Ray.raycast.delete(this);
 }
 
-Player.prototype.createRay = function(maxSteps, func, options = {} ) {
-    let loc = this.location;
-    let vec = this.getViewDirection();
-    options.source = this;
-    options.dimension = this.dimension;
-    return new Ray(maxSteps, loc.x, loc.y + 1.62, loc.z, vec.x, vec.y, vec.z, func, options);
+Player.prototype.createRay = function(options = new Options()) {
+    const loc = this.location;
+    const vec = this.getViewDirection();
+    const r = new Ray({x: loc.x, y: loc.y + 1.62, z: loc.z}, vec, options);
+    r.source = this;
+    r.dimension = this.dimension;
+    return r;
 };
 
+let pastDate = Date.now();
+let now, delta;
 system.runInterval(() => {
-    for (let ray of Ray.raycast) {
-        if (ray.autoMove) {
-        let i1 = Math.floor(ray.step + ray.stepsPerTick) - Math.floor(ray.step);
-        for(let i=0;i<i1 && ray;i++) {
-            ray.move(true);
-        }
-        ray.step += ray.stepsPerTick - i1;
-        }
+    now = Date.now();
+    delta = (now - pastDate)/1000;
+    pastDate = now;
+    
+    for (const ray of Ray.raycast) {
+        ray.options._moveFunction(ray);
     }
-}, 0)
+}, 0);
+
+function _tick (ray) {
+    ray._tick += ray.options.stepsPerTick;
+    const i1 = Math.floor(ray._tick);
+    ray._tick -= i1;
+        for(let i=0;i<i1 && ray.isActive;i++) {
+            if (ray.canMove)
+            ray.move(true);
+            else ray.options.func(ray);
+        }
+}
+
+function _time (ray) {
+    ray._tick += ray.options.stepsPerSecond * delta
+    const i1 = Math.floor(ray._tick);
+    ray._tick -= i1;
+        for(let i=0;i<i1 && ray.isActive;i++) {
+            if (ray.canMove)
+            ray.move(true);
+            else ray.options.func(ray);
+        }
+}
